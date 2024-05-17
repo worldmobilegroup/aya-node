b1bed0c8249c67d0e6816cc89b095f0b1e7c8db3
JAM <94838432+ny4rl4th0t3p@users.noreply.github.com>
2024-05-17 10:33:56 +0200

    Update "aya-runtime" package and specification versions

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -144,7 +144,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("aya-node"),
     impl_name: create_runtime_str!("aya-node"),
     authoring_version: 1,
-    spec_version: 1,
+    spec_version: 2,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
